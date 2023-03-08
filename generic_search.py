111f7424975c73586eaa692ff92d938d0ab09170
oopsitmelted <sstickeler@gmail.com>
2023-03-08 18:04:16 +0000

    Implemented BFS maze search

--- generic_search.py
+++ generic_search.py
@@ -64,6 +64,23 @@ class Stack(Generic[T]):
     def __repr__(self) -> str:
         return repr(self._container)
 
+class Queue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: Deque[T] = Deque()
+
+    @property
+    def empty(self) -> bool:
+        return not self._container
+    
+    def push(self,item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.popleft()
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
 class Node(Generic[T]):
     def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0) -> None:
         self.state: T = state
@@ -76,6 +93,26 @@ class Node(Generic[T]):
 
 def dfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]]) -> Optional[Node[T]]:
     frontier: Stack[Node[T]] = Stack()
+    frontier.push(Node(initial, None))
+    explored: Set[T] = {initial}
+
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+
+        if goal_test(current_state):
+            return current_node
+
+        for child in successors(current_state):
+            if child in explored:
+                continue
+            explored.add(child)
+            frontier.push((Node(child, current_node)))
+
+    return None
+
+def bfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]]) -> Optional[Node[T]]:
+    frontier: Queue[Node[T]] = Queue()
     frontier.push(Node(initial, None))
     explored: Set[T] = {initial}
 
